cae148a6ac73236f6d7e47cba921f0333eb0f623
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-05-19 19:39:41 +0545

    make a dummy route

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.get('/', (req, res) => {
-   res.send({'hi': 'there'})
-});
+app.use(bodyParser.json());
+
+require('./routes/dialogFlowRoutes')(app);
+
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, ()=>{
  console.log("Server started at port "+ PORT)
 });  
