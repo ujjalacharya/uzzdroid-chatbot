b57d2efd984e6d2f5d84680870a57dcf65f45dba
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-05-25 09:11:20 +0545

    make the text-response function async

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,49 +1,37 @@
-const dialogflow = require('dialogflow');
-const config = require('../config/keys');
+const dialogflow = require("dialogflow");
+const config = require("../config/keys");
 
 const sessionClient = new dialogflow.SessionsClient();
 
-const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
+const sessionPath = sessionClient.sessionPath(
+  config.googleProjectID,
+  config.dialogFlowSessionID
+);
 
 module.exports = app => {
+  app.get("/", (req, res) => {
+    res.send({ hello: "Johnny" });
+  });
 
- app.get('/', (req, res) => {
-     res.send({'hello': 'Johnny'})
- });
-
- app.post('/api/df_text_query', (req, res) => {
+  app.post("/api/df_text_query", async (req, res) => {
     const request = {
-        session: sessionPath,
-        queryInput: {
-          text: {
-            // The query to send to the dialogflow agent
-            text: req.body.text,
-            // The language used by the client (en-US)
-            languageCode: config.dialogFlowSessionLanguageCode,
-          },
-        },
-      };
+      session: sessionPath,
+      queryInput: {
+        text: {
+          // The query to send to the dialogflow agent
+          text: req.body.text,
+          // The language used by the client (en-US)
+          languageCode: config.dialogFlowSessionLanguageCode
+        }
+      }
+    };
 
-      sessionClient
-      .detectIntent(request)
-      .then(responses => {
-          console.log('Detected intent');
-          const result = responses[0].queryResult;
-          console.log(`  Query: ${result.queryText}`);
-          console.log(`  Response: ${result.fulfillmentText}`);
-          if (result.intent) {
-              console.log(`  Intent: ${result.intent.displayName}`);
-          } else {
-              console.log(`  No intent matched.`);
-          }
-      })
-      .catch(err => {
-          console.error('ERROR:', err);
-      });
-    
- });
+    let responses = await sessionClient.detectIntent(request);
 
- app.post('/api/df_event_query', (req, res) => {
-     res.send({'do': 'event query'})
- });
-}
+    res.send(responses[0].queryResult);
+  });
+
+  app.post("/api/df_event_query", (req, res) => {
+    res.send({ do: "event query" });
+  });
+};
